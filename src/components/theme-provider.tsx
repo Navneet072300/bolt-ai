a470c2ca399ab8af5b3a48c027add26179a76f3b
Navneet Shahi <navneetshahi345@gmail.com>
2025-05-02 22:57:29 +0530

    update users.ts and theme-provider.tsx

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -5,13 +5,45 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { MessagesContext } from "@/context/MessageContext";
 import { UserDetailContext } from "@/context/UserDetailContext";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { useConvex } from "convex/react";
+import { api } from "../../convex/_generated/api";
+import { Id } from "../../convex/_generated/dataModel";
+
+interface User {
+  _id: Id<"user">;
+  _creationTime: number;
+  picture: string;
+  name: string;
+  email: string;
+  uid: string;
+}
 
 export function ThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
   const [messages, setMessages] = React.useState();
-  const [userDetail, setUserDetail] = React.useState();
+  const [userDetail, setUserDetail] = React.useState<User | undefined>(
+    undefined
+  );
+  const convex = useConvex();
+
+  React.useEffect(() => {
+    IsAuthenticated();
+  }, []);
+
+  const IsAuthenticated = async () => {
+    if (typeof window !== "undefined") {
+      const userItem = localStorage.getItem("user");
+      if (userItem !== null) {
+        const user = JSON.parse(userItem);
+        const result = await convex.query(api.users.GetUser, {
+          email: user?.email,
+        });
+        setUserDetail(result);
+      }
+    }
+  };
 
   return (
     <GoogleOAuthProvider
